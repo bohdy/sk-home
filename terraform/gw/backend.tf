8775859eefc8224992c4d883dd0e37aadc403d4c
Viktor Bohdal <viktor@bohdal.name>
2026-04-25 22:43:22 +0200

    secret name

--- terraform/gw/backend.tf
+++ terraform/gw/backend.tf
@@ -6,10 +6,17 @@ terraform {
 
     use_lockfile = true
 
-    skip_region_validation      = true
+    # skip_region_validation      = true
+    # skip_credentials_validation = true
+    # skip_metadata_api_check     = true
+    # skip_requesting_account_id  = true
+
     skip_credentials_validation = true
     skip_metadata_api_check     = true
+    skip_region_validation      = true
     skip_requesting_account_id  = true
+    skip_s3_checksum            = true
+    use_path_style              = true
 
     endpoints = {
       s3 = "https://3b4089f3bd57e01c9d7c03c2587c3436.r2.cloudflarestorage.com"
